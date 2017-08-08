c8b3fab6eb4910ceda5645b94ebc9d0650540661
Wang Bing <wangbing1985@163.com>
2017-08-08 14:43:57 +0800

    Create ftk_load.js
    Quote : http://blog.csdn.net/ldl_xz/article/details/51765317

---
+++ ftk_load.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+!window.jQuery && document.write('<script src=http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js><\/script>');
+</script>
